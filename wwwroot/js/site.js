a3a79f4a69ffc06db757ec9b5c01d39b75602dec
kxlmtdx <nailslevens@gmail.com>
2025-03-05 22:51:57 +0300

    0.1.0 key to start part 2

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Flatpickr
+flatpickr("#date", {
+    dateFormat: "d.m.Y",
+    locale: "ru",
+    minDate: "today",
+});
+
+// Swiper
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
