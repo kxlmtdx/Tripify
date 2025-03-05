089416aa323b991c5aaf6eddccccd3a4c29f7109
kxlmtdx <nailslevens@gmail.com>
2025-03-05 21:38:38 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TourFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
