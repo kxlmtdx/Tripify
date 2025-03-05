805a1050106a99a4cb5239e00cd1a3d22277daf8
kxlmtdx <nailslevens@gmail.com>
2025-03-05 22:48:40 +0300

    0.1.0 key to start

---
+++ Controllers/SupportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TourFlow.Controllers
+{
+    public class SupportController : Controller
+    {
+        public IActionResult Support()
+        {
+            return View();
+        }
+    }
+}
